b9f133b1906cca1e5f15ca90702df2d1ccf5b0c1
Rashawn Raiford <raiford87@gmail.com>
2020-10-11 23:21:56 -0400

    Added functionaly to get current weather from openweather api

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ $(document).ready(function(){
 
     var root = "https://api.openweathermap.org/data/2.5/";
     
+    function getWeather(city) {
+    
+        var urlcurrent = root + "/weather?q=" + city + "&units=imperial&appid=6134970242bfd5a0d02311fb56d60846";
+    
+        $.ajax({
+          url: urlcurrent,
+          method: "GET",
+        }).then(function (response) {
+          console.log(response); 
+          var date = new Date(response.dt * 1000);
+          var weatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+    
+          $("#city").text(response.name);
+          $("#city").append(" <span>(" + date.toLocaleDateString() + ")");
+          $("#city").append('<img class="weather-icon" alt="' + response.weather[0].description + '" src=' + weatherIcon + ">");
+          $("#current-temp").text(response.main.temp + " °F");
+          $("#current-humidity").text(response.main.humidity + "%");
+          $("#current-wind-speed").text(response.wind.speed + " MPH");
+        })
+    }
+
     $("form").on("submit", function (event) {
         event.preventDefault();
         var city = $("#searchInput").val();
@@ -16,6 +37,8 @@ $(document).ready(function(){
           );
 
           $("#searchContainer").append(historyBtn);
+
+          getWeather(city);
 
       });
 })
