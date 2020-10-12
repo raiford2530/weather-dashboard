df41e7bfb9d02f110cddfb0fc6b1755f7922ce91
Rashawn Raiford <raiford87@gmail.com>
2020-10-11 23:43:47 -0400

    Added functionality to get five day forecast from openweather api

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         }).then(function (response) {
           console.log(response); 
           var date = new Date(response.dt * 1000);
-          var weatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+          var weatherIcon = getWeatherIcon(response.weather[0].icon);
     
           $("#city").text(response.name);
           $("#city").append(" <span>(" + date.toLocaleDateString() + ")");
@@ -20,7 +20,43 @@ $(document).ready(function(){
           $("#current-temp").text(response.main.temp + " °F");
           $("#current-humidity").text(response.main.humidity + "%");
           $("#current-wind-speed").text(response.wind.speed + " MPH");
-        })
+
+          getFiveDayForecast(response.coord.lat, response.coord.lon);
+
+        });
+    }
+
+    function getWeatherIcon(icon){
+        return "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    }
+
+    function getFiveDayForecast(lat, lon){
+      var url = root + "onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&units=imperial&appid=6134970242bfd5a0d02311fb56d60846";
+
+      $.ajax({
+        url: url,
+        method: "GET",
+      }).then(function (response) {
+
+        var forecasts = response.daily;
+
+        $(".five-day-forecast").empty();
+
+        for (var i = 1; i < 6; i++) {
+          var forecast = forecasts[i];
+          date = new Date(forecast.dt * 1000);
+          weatherIcon = getWeatherIcon(forecast.weather[0].icon);
+
+          var forecastHTML = "<li>";
+          forecastHTML += "<div><p class='font-weight-bold'>" + date.toLocaleDateString() + "</p>";
+          forecastHTML += '<img src="' + weatherIcon + '" alt="' + forecast.weather[0].description + '" class="five-day-weather-icon">';
+          forecastHTML += "<p>Temp: " + forecast.temp.day + " °F</p>";
+          forecastHTML += "<p>Humidity: " + forecast.humidity + "%</p></div>";
+          forecastHTML += "</li>";
+
+          $(".five-day-forecast").append(forecastHTML);
+        }
+      });
     }
 
     $("form").on("submit", function (event) {
