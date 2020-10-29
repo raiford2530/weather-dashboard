d6be2f2b59e24e9e487fcdfef6b25e8b588a6e6d
Rashawn Raiford <raiford87@gmail.com>
2020-10-29 02:51:31 -0400

    Updated code to follow ECMA 6 standards

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-  var root = "https://api.openweathermap.org/data/2.5/";
+  const root = "https://api.openweathermap.org/data/2.5/";
   const token = "6134970242bfd5a0d02311fb56d60846";
 
   //Hide the error placeholder
   $('#error').hide();
 
   //Get the last city searched from local storage if there is one and get the weather for it
-  var lastCitySearched = localStorage.getItem("lastCitySearched");
+  const lastCitySearched = localStorage.getItem("lastCitySearched");
   if (lastCitySearched) {
     getWeather(lastCitySearched);
   }
 
   //Function to get the current weather, five day forecast, and UV Index
   function getWeather(city, addHistory) {
-    var urlcurrent = root + "/weather?q=" + city + "&units=imperial&appid=" + token;
+    const urlcurrent = root + "/weather?q=" + city + "&units=imperial&appid=" + token;
 
     $.ajax({
       url: urlcurrent,
@@ -22,46 +22,38 @@ $(document).ready(function () {
         //If error is thrown from api call show in the error placeholder
         $('#error-text').text(jqXHR.responseJSON.message).show();
       },
-    }).then(function (response) {
+    }).then((response) => {
 
       //If this is a call from a search add a history button.
       //If it's a call from the last search from localstorage don't add history button
       if (addHistory) {
         $('#error-text').hide();
-        var historyBtn = $(
-          '<div class="row">' +
-            '<div class="col-sm-12">' +
-            '<button class="historyBtn btn border" data-city="' +
-            city +
-            '">' +
-            city +
-            "</button>" +
-            "</div>" +
-            "</div>"
+        const historyBtn = $(
+          `<div class="row">
+            <div class="col-sm-12">
+              <button class="historyBtn btn border" data-city="${city}">${city}</button>
+            </div>
+           </div>`
         );
 
         $("#searchContainer").append(historyBtn);
       }
 
       //Get the dt value from the response and create a Date object
-      var date = new Date(response.dt * 1000);
+      let date = new Date(response.dt * 1000);
       //Get the weather icon from the response
-      var weatherIcon = getWeatherIcon(response.weather[0].icon);
+      const weatherIcon = getWeatherIcon(response.weather[0].icon);
 
       //Set the text for the current weather details
       $("#city").text(response.name);
-      $("#city").append(" <span>(" + date.toLocaleDateString() + ")");
+      $("#city").append(` <span>(${date.toLocaleDateString()})`);
       //Add the icon for the current weather
       $("#city").append(
-        '<img class="weather-icon" alt="' +
-          response.weather[0].description +
-          '" src=' +
-          weatherIcon +
-          ">"
+        `<img class="weather-icon" alt="${response.weather[0].description}" src="${weatherIcon}>`
       );
-      $("#current-temp").text(response.main.temp + " °F");
-      $("#current-humidity").text(response.main.humidity + "%");
-      $("#current-wind-speed").text(response.wind.speed + " MPH");
+      $("#current-temp").text(`${response.main.temp} °F`);
+      $("#current-humidity").text(`${response.main.humidity}%`);
+      $("#current-wind-speed").text(`${response.wind.speed} MPH`);
 
       getFiveDayForecast(response.coord.lat, response.coord.lon);
       getUVIndex(response.coord.lat, response.coord.lon);
@@ -72,24 +64,17 @@ $(document).ready(function () {
   }
 
   function getWeatherIcon(icon) {
-    return "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    return `http://openweathermap.org/img/wn/${icon}@2x.png`;
   }
 
   function getFiveDayForecast(lat, lon) {
-    var url =
-      root +
-      "onecall?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&exclude=current,minutely,hourly,alerts&units=imperial&appid=" +
-      token;
+    var url = root + `onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${token}`;
 
     //Get the five day forecast from the onecall endpoint
     $.ajax({
       url: url,
       method: "GET",
-    }).then(function (response) {
+    }).then((response) => {
       var forecasts = response.daily;
 
       $(".five-day-forecast").empty();
@@ -101,20 +86,14 @@ $(document).ready(function () {
         weatherIcon = getWeatherIcon(forecast.weather[0].icon);
 
         //Dynamically add item for five day forecast
-        var forecastHTML = "<li>";
-        forecastHTML +=
-          "<div><p class='font-weight-bold'>" +
-          date.toLocaleDateString() +
-          "</p>";
-        forecastHTML +=
-          '<img src="' +
-          weatherIcon +
-          '" alt="' +
-          forecast.weather[0].description +
-          '" class="five-day-weather-icon">';
-        forecastHTML += "<p>Temp: " + forecast.temp.day + " °F</p>";
-        forecastHTML += "<p>Humidity: " + forecast.humidity + "%</p></div>";
-        forecastHTML += "</li>";
+        var forecastHTML = `<li>
+                              <div>
+                                <p class='font-weight-bold'>${date.toLocaleDateString()}</p>
+                                <img src="${weatherIcon}" alt="${forecast.weather[0].description}" class="five-day-weather-icon">
+                                <p>Temp: ${forecast.temp.day} °F</p>
+                                <p>Humidity: ${forecast.humidity}%</p>
+                              </div>
+                            </li>`;
 
         $(".five-day-forecast").append(forecastHTML);
       }
@@ -122,15 +101,15 @@ $(document).ready(function () {
   }
 
   function getUVIndex(lat, lon) {
-    var url = root + "uvi?lat=" + lat + "&lon=" + lon + "&appid=" + token;
+    const url = root + `uvi?lat=${lat}&lon=${lon }&appid=${token}`;
 
     //Get the UV index for the city
     $.ajax({
       url: url,
       method: "GET",
-    }).then(function (response) {
-      var uvIndex = response.value;
-      var indicatorColor;
+    }).then((response) => {
+      const uvIndex = response.value;
+      let indicatorColor;
       $("#current-uv-index").text(uvIndex);
 
       //Determine the indicator for the UV Index
@@ -151,9 +130,9 @@ $(document).ready(function () {
   }
 
   //Add the submit event handler for the form 
-  $("form").on("submit", function (event) {
+  $("form").on("submit", (event) => {
     event.preventDefault();
-    var city = $("#searchInput").val();
+    const city = $("#searchInput").val();
     $("#searchInput").val("");
 
     //Check to see if the input is not blank
@@ -163,7 +142,7 @@ $(document).ready(function () {
   });
 
   //Add the click event handler for the history buttons
-  $(document).on("click", ".historyBtn", function () {
+  $(document).on("click", ".historyBtn", function(){
     getWeather($(this).attr("data-city"));
   });
 });
